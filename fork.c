15ad2e61ccadc8f8b7d6256c3607c4fa6635f278
malikaa1 <rahmanimalika0@gmail.com>
2022-02-06 22:27:10 +0100

    fix 1 data race

--- fork.c
+++ fork.c
@@ -12,10 +12,17 @@
 
 #include "philo.h"
 
-void take_fork(t_philo *philo)
+void	log_take_fork(t_philo *philo)
 {
-	int id;
-	int left_id;
+	pthread_mutex_lock(&philo->info->print_lock);
+	log_taking_fork(philo);
+	pthread_mutex_unlock(&philo->info->print_lock);
+}
+
+void	take_fork(t_philo *philo)
+{
+	int	id;
+	int	left_id;
 
 	id = philo->id - 1;
 	left_id = philo->id;
@@ -25,17 +32,17 @@ void take_fork(t_philo *philo)
 	{
 		pthread_mutex_lock(&philo->info->fork_locks[id]);
 		if (can_run(philo) == 1 && is_still_alive(philo))
-			log_taking_fork(philo);
+			log_take_fork(philo);
 		pthread_mutex_lock(&philo->info->fork_locks[left_id]);
 		if (can_run(philo) == 1 && is_still_alive(philo))
-			log_taking_fork(philo);
+			log_take_fork(philo);
 	}
 }
 
-void drop_fork(t_philo *philo)
+void	drop_fork(t_philo *philo)
 {
-	int id;
-	int left_id;
+	int	id;
+	int	left_id;
 
 	id = philo->id - 1;
 	left_id = philo->id;
